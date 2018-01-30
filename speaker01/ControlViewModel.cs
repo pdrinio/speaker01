988faad337be73ea746c12673f8f233b7160420e
Pedro Orgeira <poc.desarrollo@hotmail.com>
2018-01-30 19:54:14 +0100

    Estructura MVVM

---
+++ speaker01/ControlViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace speaker01
+{
+    class ControlViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ControlViewModel(MediaElement mediaElement)
+        {
+
+        }
+    }
+}
